547428db7431290b2aa4b80d6164f96ef95305f5
rybakovmax23 <rybakovmax23@gmail.com>
2023-02-07 23:16:25 +0300

    feat: add webpack to the project

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Wrapper } from "./style";
+
+const App = () => {
+    return (
+        <Wrapper>
+            <div>Hello React</div>
+        </Wrapper>
+    )
+}
+export default App
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import './index.css';
+import React from 'react';
+import { render } from 'react-dom'
+import App from 'AliasExample/App';
+
+
+
+render(<App/>, document.getElementById("root"))
